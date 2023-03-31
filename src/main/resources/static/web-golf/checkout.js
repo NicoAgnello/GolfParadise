d81c2347c1a5beeee5911cf1ff0a0c0c4bd7a8bd
Nicolas Agnello <nikoagnello1@gmail.com>
2023-03-31 11:14:55 -0300

    fixed merge problem with checkout.js

--- src/main/resources/static/web-golf/checkout.js
+++ src/main/resources/static/web-golf/checkout.js
@@ -51,35 +51,35 @@ createApp({
         this.total = JSON.parse(localStorage.getItem("total"));
       }
       axios("/api/products")
-        .then((response) => {
-          console.log(response);
-          this.products = response.data;
-          this.cart.forEach((order) => {
-            this.products.forEach((prod) => {
-              if (prod.id == order.id) {
-                order.price = prod.price;
-                if (prod.stock - order.quantity >= 0) {
-                  order.stock = prod.stock - order.quantity;
-                } else {
-                  this.stockInconsistency = true;
+          .then((response) => {
+            console.log(response);
+            this.products = response.data;
+            this.cart.forEach((order) => {
+              this.products.forEach((prod) => {
+                if (prod.id == order.id) {
+                  order.price = prod.price;
+                  if (prod.stock - order.quantity >= 0) {
+                    order.stock = prod.stock - order.quantity;
+                  } else {
+                    this.stockInconsistency = true;
+                  }
                 }
-              }
+              });
             });
-          });
-          if (this.stockInconsistency) {
-            console.log("Stock inconsistency.");
-            localStorage.clear();
-            Swal.fire({
-              showConfirmButton: false,
-              timer: 2000,
-              timerProgressBar: true,
-              icon: "error",
-              title: `Stock inconsistency`,
-            });
-            setTimeout(() => location.replace("/web-golf/mockup.html"), 2000);
-          }
-        })
-        .catch((error) => console.log(error));
+            if (this.stockInconsistency) {
+              console.log("Stock inconsistency.");
+              localStorage.clear();
+              Swal.fire({
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                icon: "error",
+                title: `Stock inconsistency`,
+              });
+              setTimeout(() => location.replace("/web-golf/mockup.html"), 2000);
+            }
+          })
+          .catch((error) => console.log(error));
     },
     checkProductInCart(product) {
       return this.cart.find((prod) => product.id === prod.id);
@@ -108,16 +108,16 @@ createApp({
     },
     removeProduct(product) {
       axios("/api/products")
-        .then((response) => {
-          console.log(response);
-          product.stock = response.data.find((prod) => prod.id === product.id).stock;
-          product.quantity = 0;
-          this.cart.splice(this.cart.indexOf(product), 1);
-          this.getTotal();
-          localStorage.setItem("cart", JSON.stringify(this.cart));
-          localStorage.setItem("total", JSON.stringify(this.total));
-        })
-        .catch((error) => console.log(error));
+          .then((response) => {
+            console.log(response);
+            product.stock = response.data.find((prod) => prod.id === product.id).stock;
+            product.quantity = 0;
+            this.cart.splice(this.cart.indexOf(product), 1);
+            this.getTotal();
+            localStorage.setItem("cart", JSON.stringify(this.cart));
+            localStorage.setItem("total", JSON.stringify(this.total));
+          })
+          .catch((error) => console.log(error));
     },
     emptyCart() {
       localStorage.clear();
@@ -135,10 +135,10 @@ createApp({
       this.v$.deliveryAddress.$touch();
       this.v$.zipCode.$touch();
       if (
-        !this.v$.cardNumber.$invalid &&
-        !this.v$.cardCvv.$invalid &&
-        !this.v$.deliveryAddress.$invalid &&
-        !this.v$.zipCode.$invalid
+          !this.v$.cardNumber.$invalid &&
+          !this.v$.cardCvv.$invalid &&
+          !this.v$.deliveryAddress.$invalid &&
+          !this.v$.zipCode.$invalid
       ) {
         this.pay();
       }
@@ -146,11 +146,11 @@ createApp({
     getDeliveryCost() {
       console.log(this.zipCode);
       axios
-        .post("/api/clients/current/delivery-cost", `zipCode=${this.zipCode}`)
-        .then((response) => {
-          this.deliveryCost = response.data.deliveryCost;
-        })
-        .catch((error) => console.log(error));
+          .post("/api/clients/current/delivery-cost", `zipCode=${this.zipCode}`)
+          .then((response) => {
+            this.deliveryCost = response.data.deliveryCost;
+          })
+          .catch((error) => console.log(error));
     },
     pay() {
       const orders = this.cart.reduce((acc, curr) => {
@@ -162,68 +162,69 @@ createApp({
         return acc;
       }, []);
       axios
-        .post("https://mindhub-brothers.up.railway.app/api/cards/pay", {
-          cardNumber: this.cardNumber,
-          cardCvv: this.cardCvv,
-          amount: this.total + this.deliveryCost,
-          description: "Golf Paradise purchase.",
-        })
-        .then((response) => {
-          console.log(response);
-          axios
-            .post("/api/clients/current/order-purchases/generate", { orderProducts: orders })
-            .then((response) => {
-              console.log(response);
-              window.location.href = "/api/clients/current/pdf/generate";
-              localStorage.clear();
-              Swal.fire({
-                showConfirmButton: false,
-                timer: 2000,
-                timerProgressBar: true,
-                icon: "success",
-                background: "#FCE6BE",
-                title: `Purchase successful`,
-              });
-              setTimeout(() => location.replace("/web-golf/contact.html"), 2000);
-            })
-            .catch((error) => console.log(error));
-        })
-        .catch((error) => {
-          console.log(error);
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: error.response.data,
-            background: "#FCE6BE",
-            confirmButtonColor: "#198754",
+          .post("https://mindhub-brothers.up.railway.app/api/cards/pay", {
+            cardNumber: this.cardNumber,
+            cardCvv: this.cardCvv,
+            amount: this.total + this.deliveryCost,
+            description: "Golf Paradise purchase.",
+          })
+          .then((response) => {
+            console.log(response);
+            axios
+                .post("/api/clients/current/order-purchases/generate", { orderProducts: orders, deliveryAddress: this.deliveryAddress, deliveryCost: this.deliveryCost })
+                .then((response) => {
+                  console.log(response);
+                  window.location.href = "/api/clients/current/pdf/generate";
+                  localStorage.clear();
+                  Swal.fire({
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                    icon: "success",
+                    background: "#FCE6BE",
+                    title: `Purchase successful`,
+                  });
+                  setTimeout(() => location.replace("/web-golf/contact.html"), 2000);
+                })
+                .catch((error) => console.log(error));
+          })
+          .catch((error) => {
+            console.log(error);
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: error.response.data,
+              background: "#FCE6BE",
+              confirmButtonColor: "#198754",
+            });
           });
-        });
     },
     logOut() {
       axios
-        .post("/api/logout")
-        .then(() => {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener("mouseenter", Swal.stopTimer);
-              toast.addEventListener("mouseleave", Swal.resumeTimer);
-            },
+          .post("/api/logout")
+          .then(() => {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 2000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+              },
+            });
+
+            Toast.fire({
+              icon: "warning",
+              title: "Log out successfully",
+            }).then(() => {
+              location.href = "./landing.html";
+            });
+          })
+          .catch((err) => {
+            console.log(err);
           });
-          Toast.fire({
-            icon: "warning",
-            title: "Log out successfully",
-          }).then(() => {
-            location.href = "./landing.html";
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
     },
   },
 }).mount("#app");
