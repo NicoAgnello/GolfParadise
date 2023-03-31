e6d299f7cbc49a2795819d905b99387196cda397
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-03-31 02:11:15 -0300

    add products title, improve purchase process

--- src/main/java/com/mindhub/golfparadise/dtos/PurchaseDTO.java
+++ src/main/java/com/mindhub/golfparadise/dtos/PurchaseDTO.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 
 public class PurchaseDTO {
     private ArrayList<OrderProductDTO> orderProducts = new ArrayList<>();
+    private String deliveryAddress;
 
     public PurchaseDTO() {};
 
-    public PurchaseDTO(ArrayList<OrderProductDTO> orderProducts) {
+    public PurchaseDTO(ArrayList<OrderProductDTO> orderProducts, String deliveryAddress) {
         this.orderProducts = orderProducts;
+        this.deliveryAddress = deliveryAddress;
     }
 
     public ArrayList<OrderProductDTO> getOrderProducts() {
         return orderProducts;
     }
 
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
 }
-
